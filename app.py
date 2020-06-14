e5bdf049c82f0681435c5b3bbaf514221701e596
psnehas <sneha@sjsu.edu>
2020-06-14 14:30:36 -0700

    register form

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import sqlite3
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegisterBookseller
+from forms import RegisterBookseller,RegisterCustomer
+from database_manager import SQLiteConnection
+
 app = Flask(__name__)
 # trying to make a function that can be used to create/connect to customers.db, booksellers.db and books.db. 
 # then creating three different models for each of the above databases.
 # followed video tutorials: https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=136s,     https://www.youtube.com/watch?v=3mwFC4SHY-Y
-class SQLiteConnection:
-    def __init__(self):
-        try:
-            self.conn = sqlite3.connect('books.db')
-        except exception as e:
-            print(e)
-        self.c = self.conn.cursor()
+# class SQLiteConnection:
+#     def __init__(self):
+#         try:
+#             self.conn = sqlite3.connect('books.db')
+#         except exception as e:
+#             print(e)
+#         self.c = self.conn.cursor()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SECRET_KEY'] = 'any secret string'
@@ -52,12 +54,19 @@ app.config['SECRET_KEY'] = 'any secret string'
 
 
 
-@app.route('/register', methods=('GET', 'POST'))
-def register():
+@app.route('/registerSeller', methods=('GET', 'POST'))
+def registerSeller():
     form = RegisterBookseller()
     if form.validate_on_submit():
         return redirect(url_for('success'))
-    return render_template('register.html', form=form)
+    return render_template('registerSeller.html',title= "RegisterSeller", form=form)
+
+@app.route('/registerCustomer', methods=('GET', 'POST'))
+def registerCustomer():
+    form = RegisterCustomer()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('registerCustomer.html', title="RegisterCustomer", form=form)
 
 @app.route('/')
 def index():
@@ -65,3 +74,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    registerSeller()
