9e7a2cac15069060c7f869bf0e094c876e0b3f82
psnehas <sneha@sjsu.edu>
2020-06-14 21:36:15 -0700

    frontend files

--- app.py
+++ app.py
@@ -1,65 +1,30 @@
 import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegisterBookseller,RegisterCustomer
+from forms import RegisterBookseller,RegisterCustomer,LoginSeller
 from database_manager import SQLiteConnection
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-# trying to make a function that can be used to create/connect to customers.db, booksellers.db and books.db. 
-# then creating three different models for each of the above databases.
-# followed video tutorials: https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=136s,     https://www.youtube.com/watch?v=3mwFC4SHY-Y
-# class SQLiteConnection:
-#     def __init__(self):
-#         try:
-#             self.conn = sqlite3.connect('books.db')
-#         except exception as e:
-#             print(e)
-#         self.c = self.conn.cursor()
 
+# followed video tutorials: https://www.youtube.com/watch?v=Z1RJmh_OqeA&t=136s,     https://www.youtube.com/watch?v=3mwFC4SHY-Y
+
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SECRET_KEY'] = 'any secret string'
-# db_customer = SQLAlchemy(app)
-# db_bookseller = SQLAlchemy(app)
-# db_books = SQLAlchemy(app)
-
-# class CustomerData(db_customer.Model):
-#     cid = db_customer.Column(db_customer.Integer, primary_key = True)
-#     firstName = db_customer.Column(db_customer.String(200), nullable = False)
-#     LastName = db_customer.Column(db_customer.String(200), nullable = False)
-#     customerEmail = db_customer.Column(db_customer.String(200))
-#     customerPassword = db_customer.Column(db_customer.String(20), nullable = False)
-#     customerZipCode = db_customer.Column(db_customer.Integer, nullable = False)
-
-#     def __repr__(self):
-#         return '<Task %r>' %self.cid
-
-# class BookSellerData(db_bookseller.Model):
-#     bsid = db_bookseller.Column(db_bookseller.Integer, primary_key = True)
-#     storeName = db_bookseller.Column(db_bookseller.String(400), nullable = False)
-#     storeLocation = db_bookseller.Column(db_bookseller.String(600), nullable = False)
-#     storeOwner = db_bookseller.Column(db_bookseller.String(100), nullable = False)
-#     storeContactNum = db_bookseller.Column(db_bookseller.Integer)
-#     storeOwnerEmail = db_bookseller.Column(db_bookseller.String(100), nullable = False)
-    
-#     def __repr__(self):
-#         return '<Task %r>' %self.bsid
-        
-# class Books(db_books.Model):
-#     bid = db_books.Column(db_books.Integer, primary_key = True)
-#     bookName = db_books.Column(db_books.String(200), nullable = False)
-#     authorName = db_books.Column(db_books.String(200), nullable = False)
-#     bookCategory = db_books.Column(db_books.String(100), nullable = False)
-#     bookPrice = db_books.Column(db_books.Float, nullable = False)
-
-
-
 
 @app.route('/registerSeller', methods=('GET', 'POST'))
 def registerSeller():
+    sellerDetails = []
     form = RegisterBookseller()
-    if form.validate_on_submit():
-        return redirect(url_for('success'))
-    return render_template('registerSeller.html',title= "RegisterSeller", form=form)
+    for x in form:
+        sellerDetails.append(x.data)
+    database = SQLiteConnection()
+    database.add_seller(sellerDetails)
+    database.select_all_booksellers()
+    database.close_connection()
+    if request.method=='POST':
+        return redirect(url_for("loginSeller"))
+    return render_template('registerSeller.html',title= "RegisterSeller",form=form)
 
 @app.route('/registerCustomer', methods=('GET', 'POST'))
 def registerCustomer():
@@ -68,9 +33,18 @@ def registerCustomer():
         return redirect(url_for('success'))
     return render_template('registerCustomer.html', title="RegisterCustomer", form=form)
 
+@app.route('/loginSeller', methods = ('GET','POST'))
+def loginSeller():
+    form = LoginSeller()
+    if request.method=='POST':
+        
+        return redirect(url_for("loginSeller"))
+
 @app.route('/')
 def index():
     return render_template('landingPage.html')  
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,16 +1,32 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired, Length,Email
+from flask_wtf import FlaskForm,RecaptchaField
+from wtforms import StringField, TextField, SubmitField,PasswordField,SelectField,DateField
+from wtforms.validators import (DataRequired, Length,Email,EqualTo,URL)
 
 class RegisterBookseller(FlaskForm):
-    """Contact form."""
-    storeName = StringField('Store Name', [DataRequired()])
-    storeLocation = StringField('Loacation',[DataRequired()])
-    storeOwnerName = StringField('Owner Name',[DataRequired()])
-    storecontactNum = StringField('Contact Number',[DataRequired()])
-    storeOwnerEmail = StringField('Email', [Email(message=('Not a valid email address.')),DataRequired()])
-    # body = TextField('Message', [DataRequired(),Length(min=4, message=('Your message is too short.'))])   ,
-    # recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+    """Bookseller Contact form."""
+    storeName = StringField('Name of the Store', [DataRequired()])
+    storeLocation = StringField('Location',[DataRequired()])
+    storeOwnerName = StringField('Name of the owner',[DataRequired()])
+    storeContactNum = StringField('Owner Contact Number',[DataRequired()])
+    storeOwnerEmail = StringField('Email address', [Email(message=('Not a valid email address.')),DataRequired()])
+    StoreOwnerPassword = StringField('Password',[DataRequired()])
+
+class RegisterCustomer(FlaskForm):
+    """Customer contact form"""
+    firstName = StringField('First Name', [DataRequired()])
+    lastName = StringField('Last Name',[DataRequired()])
+    customerEmail = StringField('Email', [Email(message=('Not a valid email address.')),DataRequired()])
+    customerPassword = StringField('Password',[DataRequired()])
+    customerZipCode = StringField('ZipCode',[DataRequired()])
+    
+
+class CustomerSearchBar(FlaskForm):
+    """Customer search queries"""
+    pass
+
+class LoginSeller(FlaskForm):
+    emailAddress = StringField("Email", [DataRequired()])
+    password = StringField("Password",[DataRequired()])
+    
 
 
